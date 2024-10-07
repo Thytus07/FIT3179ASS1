b78e73533de12ce6a537dec8f03514ba1584e664
Thytus <tben0015@student.monash.edu>
2024-10-07 19:48:41 +1100

    radial plot

--- chart.js
+++ chart.js
@@ -9,3 +9,9 @@ vegaEmbed("#violin", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
+
+var vg_3 = "radial.vg.json";
+vegaEmbed("#radial", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
