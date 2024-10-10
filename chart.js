8feeed1bc31e8ab41b64679e2ec624dc825c6fae
Thytus <tben0015@student.monash.edu>
2024-10-10 12:38:02 +1100

    using pure.css to forat html

--- chart.js
+++ chart.js
@@ -4,6 +4,12 @@ vegaEmbed("#map", vg_1).then(function(result) {
 
 }).catch(console.error);
 
+var vg_5 = "violin.vg.json";
+vegaEmbed("#violin1", vg_5).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
 var vg_2 = "violin.vg.json";
 vegaEmbed("#violin", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
